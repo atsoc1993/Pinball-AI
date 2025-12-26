739e10e4d4469e5658f455ba65ff28fba094dc32
atsoc1993 <atsoc1993@gmail.com>
2025-12-26 12:54:05 -0500

    Added game ball

--- pinball-ai/src/App.tsx
+++ pinball-ai/src/App.tsx
@@ -6,6 +6,8 @@ export default function App() {
   // 500 / 30 = 20, horizontal game bar positions in intervals of 20
   // whatever the predicted game bar position is, we set pixels to the product of this number & 30
   // eg target position for game bar of 5 will set the "left" style property to "150px"
+  const gameBall = useRef<HTMLDivElement | null>(null)
+
   return (
     // Game Box
     <div style={{ width: '500px', height: '500px', backgroundColor: 'black' }}>
@@ -16,6 +18,11 @@ export default function App() {
         left: '0px', top: '400px'
       }}
       ref={gameBar} />
+
+      {/* game ball */}
+      <div style={{ width: '50px', height: '50px', borderRadius: '50px', backgroundColor: 'white'}}>
+
+      </div>
     </div>
   )
 }
