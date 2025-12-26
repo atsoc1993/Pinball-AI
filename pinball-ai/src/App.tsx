5cb617927d0e2b09839eb21f26b8338f9a1b9321
atsoc1993 <atsoc1993@gmail.com>
2025-12-26 14:42:32 -0500

    Added logic to have the game ball continue falling if the game bar is not in a qualifying position. Added a "Failed" state and changed the default position of the game bar for testing

--- pinball-ai/src/App.tsx
+++ pinball-ai/src/App.tsx
@@ -10,6 +10,8 @@ export default function App() {
   // eg target position for game bar of 5 will set the "left" style property to "150px"
   const gameBall = useRef<HTMLDivElement | null>(null)
 
+  const [failed, setFailed] = useState<boolean>(false);
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,43 +23,59 @@ export default function App() {
 
       const newTopValue = getNewTopValue(topPosition);
       const newLeftValue = getNewLeftValue(leftPosition);
-      
+
       adjustBallPosition(newTopValue, newLeftValue);
-      checkFallingState(newTopValue);
+      checkFallingState(newTopValue, gameBallPos);
       checkMovingRightState(newLeftValue);
     }, 1)
 
     return () => clearInterval(interval)
 
-  }, [falling, movingRight])
+  }, [falling, movingRight, failed])
 
   const getNewTopValue = (topPosition: number): number => {
-      const gravityDelta = 1 * ((topPosition - 98) / 50) 
-      if (topPosition < 350 && falling) {
-        return topPosition + gravityDelta        
-      } else {
-        return topPosition - gravityDelta
-      }
+    const gravityDelta = 1 * ((topPosition - 98) / 50)
+    if (falling) {
+      return topPosition + gravityDelta
+    } else {
+      return topPosition - gravityDelta
+    }
   }
 
   const getNewLeftValue = (leftPosition: number): number => {
-      if (movingRight) {
-        return leftPosition + .8
-      } else {
-        return leftPosition - .8
-      }
+    if (movingRight) {
+      return leftPosition + .8
+    } else {
+      return leftPosition - .8
+    }
   }
+
   const adjustBallPosition = (newTopValue: number, newLeftValue: number): void => {
     if (!gameBall.current) return;
     gameBall.current.style.top = `${(newTopValue).toLocaleString()}px`
     gameBall.current.style.left = `${(newLeftValue).toLocaleString()}px`
-  }
-
-  const checkFallingState = (newTopValue: number): void => {
-    if (newTopValue <= 100) setFalling(true);
-    if (newTopValue >= 350) setFalling(false);
     return
   }
+
+  const checkFallingState = (newTopValue: number, gameBallPos: DOMRect): void => {
+    if (newTopValue <= 100) setFalling(true);
+    if (newTopValue >= 350) {
+      const madeImpact = gameBarPresentForImpact(gameBallPos);
+      console.log(madeImpact)
+      if (madeImpact) setFalling(false);
+    }
+    if (newTopValue > 500) setFailed(true);
+    return
+  }
+
+  const gameBarPresentForImpact = (gameBallPos: DOMRect): boolean => {
+    if (!gameBar.current) return false;
+    const gameBarPos = gameBar.current.getBoundingClientRect();
+    const [qualifyingRangeLow, qualifyingRangeHigh] = [gameBallPos.left - 25, gameBallPos.left + 25]
+    if ((qualifyingRangeLow <= gameBarPos.left) && (gameBarPos.left <= qualifyingRangeHigh)) return true;
+    return false;
+  }
+
   const checkMovingRightState = (newLeftValue: number): void => {
     if (newLeftValue <= 0) setMovingRight(true);
     if (newLeftValue >= 450) setMovingRight(false);
@@ -66,20 +84,27 @@ export default function App() {
 
   return (
     // Game Box
-    <div style={{ width: '500px', height: '500px', backgroundColor: 'black' }}>
+    <div style={{ width: '500px', height: '500px', backgroundColor: 'black', display: 'flex'}}>
       {/* // Game Bar  */}
-      <div style={{
-        width: '50px', height: '20px', backgroundColor: 'white',
-        position: 'fixed',
-        left: '0px', top: '400px'
-      }}
-        ref={gameBar} />
+      {!failed ?
+        <>
+          <div style={{
+            width: '50px', height: '20px', backgroundColor: 'white',
+            position: 'fixed',
+            left: '200px', top: '400px'
+          }}
+            ref={gameBar} />
 
-      {/* game ball */}
-      <div style={{ width: '50px', height: '50px', borderRadius: '50px', backgroundColor: 'white', top: '100px', position: 'fixed' }}
-        ref={gameBall}>
+          {/* game ball */}
+          <div style={{ width: '50px', height: '50px', borderRadius: '50px', backgroundColor: 'white', top: '100px', position: 'fixed' }}
+            ref={gameBall}>
 
-      </div>
+          </div>
+        </>
+        : <div style={{display: 'flex', justifyContent: 'center', flex: '1 1 100%'}}>
+          <h1 style={{color: 'white', margin: 0, alignSelf: 'center'}}>Failed</h1>
+          </div>
+      }
     </div>
   )
 }
