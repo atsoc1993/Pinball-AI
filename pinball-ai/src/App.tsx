dcd297cc084f42dc85c9733a5ba4c9c03c365f2b
Leo Costa <atsoc1993@gmail.com>
2025-12-25 23:57:47 -0500

    Added game box & game bar

--- pinball-ai/src/App.tsx
+++ pinball-ai/src/App.tsx
@@ -1,8 +1,18 @@
+import { useRef } from "react"
+
 export default function App() {
+
+  const gameBar = useRef<HTMLDivElement | undefined>(undefined)
+
   return (
     // Game Box
-    <div style={{width: '500px', height: '500px', backgroundColor: 'black'}}>
-
+    <div style={{ width: '500px', height: '500px', backgroundColor: 'black' }}>
+      {/* // Game Bar  */}
+      <div style={{
+        width: '50px', height: '20px', backgroundColor: 'white',
+        position: 'fixed',
+        left: '0px', top: '400px'
+      }} />
     </div>
   )
 }
