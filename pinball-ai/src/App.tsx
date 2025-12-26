0eb6d9ed89db665fbc553163c87615201bd1344e
Leo Costa <atsoc1993@gmail.com>
2025-12-26 00:00:54 -0500

    Added some notes regarding game bar position targets and pixel positioning

--- pinball-ai/src/App.tsx
+++ pinball-ai/src/App.tsx
@@ -3,7 +3,9 @@ import { useRef } from "react"
 export default function App() {
 
   const gameBar = useRef<HTMLDivElement | undefined>(undefined)
-
+  // 500 / 30 = 20, horizontal game bar positions in intervals of 20
+  // whatever the predicted game bar position is, we set pixels to the product of this number & 30
+  // eg target position for game bar of 5 will set the "left" style property to "150px"
   return (
     // Game Box
     <div style={{ width: '500px', height: '500px', backgroundColor: 'black' }}>
