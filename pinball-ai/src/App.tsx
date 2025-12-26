0662c5e1b288c48b23913e573495557f8a8052f2
atsoc1993 <atsoc1993@gmail.com>
2025-12-26 12:52:12 -0500

    Added useRef, corrected gameBar ref type

--- pinball-ai/src/App.tsx
+++ pinball-ai/src/App.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react"
 
 export default function App() {
 
-  const gameBar = useRef<HTMLDivElement | undefined>(undefined)
+  const gameBar = useRef<HTMLDivElement | null>(null)
   // 500 / 30 = 20, horizontal game bar positions in intervals of 20
   // whatever the predicted game bar position is, we set pixels to the product of this number & 30
   // eg target position for game bar of 5 will set the "left" style property to "150px"
@@ -14,7 +14,8 @@ export default function App() {
         width: '50px', height: '20px', backgroundColor: 'white',
         position: 'fixed',
         left: '0px', top: '400px'
-      }} />
+      }}
+      ref={gameBar} />
     </div>
   )
 }
